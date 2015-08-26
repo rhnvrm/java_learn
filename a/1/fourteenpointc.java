7b1ceff2091b639eead5e08ab59e0b43c7d4c429
rhnvrm <rohanverma2004@gmail.com>
2015-08-27 00:09:09 +0530

    fixed 14c

--- a/1/fourteenpointc.java
+++ a/1/fourteenpointc.java
@@ -23,15 +23,21 @@ public class fourteenpointc {
 
 		num = s.nextInt();
 
-		for(int i = 0; i < num; i++){
-			for(int j = (num - i - 1)/2; j < num; j++){
-				System.out.print(' ');
+		// 4 * * * * requires 0 spaces
+		// 3  * * *  requires 1 spaces
+		// 2   * *	 requires 2 spaces	
+		// 1    *             3 spaces
+
+		for(int i = num; i > 0; i--){
+			for(int spaces = 0; spaces < (num-i); spaces++){
+				System.out.print(" ");
+			}	
+			for(int j = 0; j < i; j++){
+				System.out.print("* ");
 			}
-			for(int j = i; j < num; j++){
-				System.out.print('*');
-			}
-			
-			System.out.print('\n');
+
+			System.out.print("\n");
 		}
+
 	}
 }
